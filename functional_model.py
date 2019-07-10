2afcb48e481437e1f0ae580e9879ebbf7082c447
Nikhil <nsrazdan@gmail.com>
2019-07-10 14:04:21 -0700

    cleaning code

--- functional_model.py
+++ functional_model.py
@@ -12,27 +12,10 @@ from tensorflow.keras.optimizers import Adam
 logging.getLogger('tensorflow').disabled = True
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-#CONST VARIABLES
-
+# create MLP
 class Naive_MLP(object):
     def __init__(self, train_obs, train_act, valid_obs, valid_act):
-        '''
-        - data: the object returned from load data
-        '''
         self.train_obs = train_obs
         self.train_act = train_act
         self.valid_obs = valid_obs
         self.valid_act = valid_act
-        
-        
-    '''Create a model'''
-    def create_model(self, activations=['relu','softmax'], num_hidden_nodes = [256,128,64,20]):
-        
-        input_layer = Input(shape=(len(self.train_obs),))
-        hidden_layer = Dense(num_hidden_nodes[0], activations=activations[0])(input_layer)
-        for i in range(1,len(num_hidden_nodes)-1):
-            hidden_layer = Dense(num_hidden_nodes[i], activations=activations[0])(hidden_layer)
-
-        output_layer = Dense(num_hidden_nodes[len(num_hidden_nodes)-1], activations = activations[len(activations)-1])
-        model = Model(inputs=input_layer, outputs=output_layer)
-        return model
--- run_experiment.py
+++ run_experiment.py
@@ -11,13 +11,14 @@ from utils import parse_args, dir_utils
 from tensorflow.keras.layers import Input, Dense, Flatten
 from tensorflow.keras.models import Model
 
-#getting rid of "does not support AVX" warnings and info logs
+# getting rid of "does not support AVX" warnings and info logs
 logging.getLogger('tensorflow').disabled = True
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-#CONST VARIABLES
+# CONST VARIABLES
 DATAPATH = os.path.dirname(os.path.realpath(__file__))
 
+# configuring data to initialize (see mlp.config.gin)
 @gin.configurable
 def initialize_data(args):
     data = load_data.main(args)
@@ -28,20 +29,17 @@ def initialize_data(args):
     return train_obs, train_act, valid_obs, valid_act, test_obs, test_act, data.test_agent
 
 def main():
-    #parse arguments
+    # parse arguments
     args = parse_args.parse()
     args.datapath = DATAPATH + "/data/Hanabi-Full_2_6_150.pkl"
     args = parse_args.resolve_datapath(args)
 
-    #external configuration
+    # external configuration
     gin.external_configurable(tf.keras.optimizers.Adam, module='tensorflow.keras.optimizers')
     gin.external_configurable(tf.keras.losses.mean_squared_error, module='tensorflow.keras.losses')
     gin.parse_config_file('mlp.config.gin')
 
-    #create/load data
-    '''
-    - data: a reference to the Dataset object (refer to load_data.py)
-     '''
+    # get data, train model, then test model
     train_obs, train_act, valid_obs, valid_act, test_obs, test_act, test_agent = initialize_data(args)
     model = train.main(train_obs, train_act, valid_obs, valid_act, args)
     evaluate.main(model,test_obs, test_act, test_agent)
